45c433cb5b668920ffbc4a3925e50a468262043b
Adam Harzafi <155762328+AdamHarzafi@users.noreply.github.com>
2026-08-28 21:19:39 +0200

    AGGIORNAMENTO IMPORTANTE

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname, { extensions: ['html'] }));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
